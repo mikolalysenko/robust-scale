99700231c3c8396260407cab0db6de9a52503b23
Mikola Lysenko <mikolalysenko@gmail.com>
2013-10-05 11:06:22 -0500

    added more test cases

--- test/test.js
+++ test/test.js
@@ -6,8 +6,13 @@ require("tape")(function(t) {
 
 	t.same(robustScale([4], 2), [8])
 	t.same(robustScale([1, 1e64], 2), [2, 2e64])
-  
   t.same(robustScale([1], 1, [0,1]), [0,1])
+  
+  for(var i=0; i<100; ++i) {
+    for(var j=0; j<100; ++j) {
+      t.same(robustScale([i], j), [i*j])
+    }
+  }
 
 	t.end()
 })
